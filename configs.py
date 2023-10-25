3db6ae2bf68f601132041946ab781c72a8e8a83c
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-10-25 15:00:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "29570230"))
+    API_HASH = getenv("API_HASH", "17c54577b97263cce14f01f820a7e478")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6852169715:AAG348M5ApqTY_jKJC-FS3bNYheQJnod3jE")
+    FSUB = getenv("FSUB", "ROCKERSBACKUP")
+    CHID = int(getenv("CHID", "-1001654950491"))
+    SUDO = list(map(int, getenv("6307912661").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
