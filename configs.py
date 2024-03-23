c4a0147d00dce3c07293520ea3ef367af950d0fe
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-03-23 22:41:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "6a6df8006df3cb56edce33056d37baca")
     BOT_TOKEN = getenv("BOT_TOKEN", "6852169715:AAG348M5ApqTY_jKJC-FS3bNYheQJnod3jE")
     FSUB = getenv("FSUB", "ROCKERSBACKUP")
-    CHID = int(getenv("CHID", "-1001654950491"))
+    CHID = int(getenv("CHID", "-1002135593873"))
     SUDO = int(getenv("SUDO", "6331847574"))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
     
